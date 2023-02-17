e4e3673848b9af962fe8e2c983f97809bdd7b38c
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-17 04:58:34 +0300

    .

---
+++ backend/contracts/CryptoDevToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    //price of one crypto dev token
+    uint256 public constant tokenPrice = 0.001 ether;
+}

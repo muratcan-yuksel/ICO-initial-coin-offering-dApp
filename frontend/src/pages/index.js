83c9eed9ad81d0a0b3fea3e4673e99bfc19db658
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-22 04:24:50 +0300

    starting frontend, added constants

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,16 @@
+import { BigNumber, Contract, providers, utils } from "ethers";
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import {
+  NFT_CONTRACT_ABI,
+  NFT_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  TOKEN_CONTRACT_ADDRESS,
+} from "../constants";
+import styles from "../styles/Home.module.css";
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;

071ee3ea582ab0e508c42babfcae9897c23e98a8
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-22 04:16:14 +0300

    deployed contract

---
+++ backend/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x9bB2554409267461CaDa12105eB7Acb46CBC510E";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
--- backend/contracts/CryptoDevToken.sol
+++ backend/contracts/CryptoDevToken.sol
@@ -6,60 +6,60 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ICryptoDevs.sol";
 
 contract CryptoDevToken is ERC20, Ownable {
-    //price of one crypto dev token
+    // Price of one Crypto Dev token
     uint256 public constant tokenPrice = 0.001 ether;
-    //each NFT would give the user 10 tokens
-    //it needs to be represented as 10 * (10 **18) as ERC20 tokens are represented by the smallest denomination possible for the token
-    //by default ERC20 tokens have the smallest denomination of 10^(-18).
-    //This means, having a balance of (1) is actually equal to (10 ^ -18) tokens.
+    // Each NFT would give the user 10 tokens
+    // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
+    // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
+    // is actually equal to (10 ^ -18) tokens.
     // Owning 1 full token is equivalent to owning (10^18) tokens when you account for the decimal places.
     // More information on this can be found in the Freshman Track Cryptocurrency tutorial.
-    uint256 public constant tokensPerNFT = 10 * (10**18);
-    //the max total suuply is 1000 tokens for crypto dev tokens following the above equation
-    uint256 public constant maxTotalSupply = 10000 * (10**18);
-    //cryptodevs contract instance
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+    // the max total supply is 10000 for Crypto Dev Tokens
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
+    // CryptoDevsNFT contract instance
     ICryptoDevs CryptoDevsNFT;
-    //mapping to keep track of which tokenIds have been claimed
+    // Mapping to keep track of which tokenIds have been claimed
     mapping(uint256 => bool) public tokenIdsClaimed;
 
-    constructor(address _cryptoDevsContract) ERC20("CryptoDevToken", "CD") {
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
         CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
     }
 
     /**
-     *  Mints `amount` number of CryptoDevTokens
+     * @dev Mints `amount` number of CryptoDevTokens
      * Requirements:
      * - `msg.value` should be equal or greater than the tokenPrice * amount
      */
     function mint(uint256 amount) public payable {
-        //the value of the ether that should be equal or greate thab tokenPrice * amount
+        // the value of ether that should be equal or greater than tokenPrice * amount;
         uint256 _requiredAmount = tokenPrice * amount;
-        require(msg.value >= _requiredAmount, "Not enough ether sent");
-        //total tokens + amount <= 10000, otherwise revert the transaction
+        require(msg.value >= _requiredAmount, "Ether sent is incorrect");
+        // total tokens + amount <= 10000, otherwise revert the transaction
+        uint256 amountWithDecimals = amount * 10**18;
         require(
             (totalSupply() + amountWithDecimals) <= maxTotalSupply,
             "Exceeds the max total supply available."
         );
-        //call the internal function from Openzeppelin's ERC20 contract
+        // call the internal function from Openzeppelin's ERC20 contract
         _mint(msg.sender, amountWithDecimals);
     }
 
     /**
-     *  Mints tokens based on the number of NFT's held by the sender
+     * @dev Mints tokens based on the number of NFT's held by the sender
      * Requirements:
      * balance of Crypto Dev NFT's owned by the sender should be greater than 0
      * Tokens should have not been claimed for all the NFTs owned by the sender
      */
-
     function claim() public {
         address sender = msg.sender;
-        //get the number of cryptodev nft's held by a given sender address
+        // Get the number of CryptoDev NFT's held by a given sender address
         uint256 balance = CryptoDevsNFT.balanceOf(sender);
-        //if the balance is zero, revert the transaction
-        require(balance > 0, "You do not own any CryptoDev NFTs");
-        //amount keeps track of number of unclaimed tokenIds
+        // If the balance is zero, revert the transaction
+        require(balance > 0, "You don't own any Crypto Dev NFT");
+        // amount keeps track of number of unclaimed tokenIds
         uint256 amount = 0;
-        //loop over the balance and get the token ID owner by 'sender' at a given 'index' of its token list
+        // loop over the balance and get the token ID owned by `sender` at a given `index` of its token list.
         for (uint256 i = 0; i < balance; i++) {
             uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
             // if the tokenId has not been claimed, increase the amount
@@ -76,15 +76,22 @@ contract CryptoDevToken is ERC20, Ownable {
     }
 
     /**
-     *  withdraws all ETH sent to this contract
+     * @dev withdraws all ETH sent to this contract
      * Requirements:
      * wallet connected must be owner's address
      */
-
     function withdraw() public onlyOwner {
         uint256 amount = address(this).balance;
-        require(amount > 0, "No ETH to withdraw");
-        //calls the internal function from Openzeppelin's Ownable contract to get the owner of this contract
+        require(amount > 0, "Nothing to withdraw, contract balance empty");
+
         address _owner = owner();
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
     }
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
 }
